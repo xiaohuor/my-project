c081bc5c66c3c49c6788d473e021e999c3b270fb
xinghai <haozehua@outlook.com>
2018-04-10 20:13:33 +0800

    'start_a_new_project'

---
+++ src/components/Kity.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>I'm Kitty</h1>
+    <p>who are you?</p>
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Kitty",
+  data () {
+    return {
+      message: "I'm hungry. About you?"
+    }
+  }
+}
+</script>
+<style>
+.message{
+  text-decoration: underline;
+}
+</style>
+
